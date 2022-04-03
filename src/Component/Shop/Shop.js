65c87559766aa5fe78015185651bce73cc0e5540
MAvi-Rup <avirupmondal19@gmail.com>
2022-04-03 20:57:06 +0600

    Emazon Project With Localstoratge and Router complete partially

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -6,15 +6,33 @@ import Cart from '../Cart/Cart';
 
 import Product from '../Product/Product';
 import './Shop.css'
+import useCart from '../../hooks/useCart';
 
 const Shop = () => {
     const [products,setProducts] = useProducts()
-    const [cart,setCart]= useState([])
+    const [cart,setCart]= useCart(products)
 
-    const updateCart =(product)=>{
-        const newCart = [...cart,product]
-        setCart(newCart)
-        addToDb(product.id)
+    // const updateCart =(product)=>{
+    //     const newCart = [...cart,product]
+    //     setCart(newCart)
+    //     addToDb(product.id)
+    // }
+    const updateCart = (selectedProduct) =>{
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+        
+        setCart(newCart);
+        addToDb(selectedProduct.id);
     }
 
     return (
